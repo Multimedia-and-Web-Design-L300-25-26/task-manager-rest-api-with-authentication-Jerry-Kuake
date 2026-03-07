fdde71a670b8e211aa5d83ee474da70f2729ad53
Jerry <ejkuake@st.ug.edu.gh>
2026-03-07 15:29:37 +0000

    Completed Task Manager Authentication API

--- src/models/User.js
+++ src/models/User.js
@@ -9,9 +9,28 @@ import mongoose from "mongoose";
 // - createdAt (default Date.now)
 
 
-
 const userSchema = new mongoose.Schema({
-  // Students implement
+  name: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const User = mongoose.model("User", userSchema);
